7eebee0178ae7e98db8bb49e355945649f4a336d
unknown <maryam_lfc@hotmail.com>
2021-04-23 23:25:02 +0300

    Add footer section and page content

--- 2.Starbucks/index.js
+++ 2.Starbucks/index.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 	$('.toggle').click(function() {
 		$('.topnav').toggleClass('sidenav');
 	});
+	$('.footer-top-sm').on('click', function() {
+		$(this).siblings('ul').slideToggle(300).addClass('footer-row-content');
+	});
 });
