3ede8dbc7e3fbf5ae0a67d0a741cefce4d74b173
unknown <maryam_lfc@hotmail.com>
2021-04-24 21:09:52 +0300

    Add favorites section

--- 2.Starbucks/index.js
+++ 2.Starbucks/index.js
@@ -6,3 +6,18 @@ $(document).ready(function() {
 		$(this).siblings('ul').slideToggle(300).addClass('footer-row-content');
 	});
 });
+const clickHandler = (evt, option) => {
+	let tabContent, tabLinks, contentAnimation;
+
+	tabContent = document.getElementsByClassName('tab-content');
+	for (let tab of tabContent) tab.style.display = 'none';
+
+	tabLinks = document.getElementsByClassName('tab-link');
+	for (let tab of tabLinks) tab.className = tab.className.replace('active', '');
+
+	document.getElementById(option).style.display = 'block'; // Display the clicked tab and set it to active
+	contentAnimation = document.getElementById(option).children;
+
+	for (let item of contentAnimation) item.style.animation = 'fadeIn 1s';
+	evt.currentTarget.className += ' active';
+};
