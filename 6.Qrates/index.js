efe42b2e2455c77bd40164b17401d3db8b2ffe03
unknown <maryam_lfc@hotmail.com>
2021-05-07 23:45:46 +0300

    Add more page content

--- 6.Qrates/index.js
+++ 6.Qrates/index.js
@@ -5,4 +5,14 @@ $(document).ready(function() {
 	$('.toggle-button').on('click', function() {
 		$('.nav-toggled-menue').toggleClass('nav-sm-content-show');
 	});
+
+	let content = document.getElementsByClassName('accordion');
+
+	for (let c of content) {
+		c.addEventListener('click', function() {
+			this.classList.toggle('active');
+			let panel = this.nextElementSibling;
+			panel.style.display === 'block' ? (panel.style.display = 'none') : (panel.style.display = 'block');
+		});
+	}
 });
