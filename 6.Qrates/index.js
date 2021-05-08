dc0bb1448a3f4387dc1888fdc28e51205351e2a7
unknown <maryam_lfc@hotmail.com>
2021-05-08 18:22:33 +0300

    Add bottom page content

--- 6.Qrates/index.js
+++ 6.Qrates/index.js
@@ -16,3 +16,47 @@ $(document).ready(function() {
 		});
 	}
 });
+const clickHandler = (evt, option) => {
+	let tabContent, tabLinks, contentAnimation;
+
+	tabContent = document.getElementsByClassName('tab-content');
+	for (let tab of tabContent) tab.style.display = 'none';
+
+	tabLinks = document.getElementsByClassName('tab-link');
+	for (let tab of tabLinks) tab.className = tab.className.replace('active', '');
+
+	document.getElementById(option).style.display = 'flex';
+	evt.currentTarget.className += ' active';
+};
+
+let timer;
+
+const images = [
+	"url('./images/slider1.png')",
+	"url('./images/slider2.png')",
+	"url('./images/slider3.png')",
+	"url('./images/slider4.png')",
+	"url('./images/slider5.png')",
+	"url('./images/slider6.png')"
+];
+const slider = document.getElementById('#slider');
+const img = document.getElementsByClassName('make-it-real-img');
+let i = 0;
+
+const automaticSlideshow = () => {
+	console.log($('.make-it-real-img'));
+	img[0].style.backgroundImage = images[i];
+	i == 5 ? (i = 0) : i++;
+};
+
+let startTime = new Date().getTime();
+window.onload = function() {
+	let interval = setInterval(function() {
+		if (new Date().getTime() - startTime > 60000 * 5) {
+			//Stop after 5 minutes
+			clearInterval(interval);
+			return;
+		}
+		automaticSlideshow();
+	}, 1000);
+};
