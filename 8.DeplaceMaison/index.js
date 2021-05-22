1831092dab062ce3ed95cbf4c4b243ce9d263757
unknown <maryam_lfc@hotmail.com>
2021-05-22 20:22:26 +0300

    Navbar and css fixes

--- 8.DeplaceMaison/index.js
+++ 8.DeplaceMaison/index.js
@@ -37,4 +37,36 @@ $(document).ready(function() {
 			innerSlider.style.left = `-${inner.width - outer.width}px`;
 		}
 	}
+
+	let chkContent = document.querySelector('.names-container-right-content p');
+	let chkName = document.querySelector('.names-container-left-names h3');
+	let message =
+		'Déplacé Maison is a convincer for anticipation. The urban trekking as nevers seen before. An exceptional product that has no equal alongside a great team represent the brand professionally. &nbsp; ';
+	let message2 =
+		'Good things come to those who wait – Déplacé Maison is what has been missing in the modern fashion industry for years. Buy a shoe of high quality and design it finally happened. &nbsp;';
+	let author = 'William Gibson';
+	let author2 = 'Jamie K. Cohen';
+
+	const changeContext = (chk, txt1, txt2) => {
+		$(chk).addClass('collapsed');
+		if (chk.textContent == txt1) {
+			setTimeout(function() {
+				$(chk).removeClass('collapsed');
+				$(chk).text(txt2);
+			}, 3000);
+		} else {
+			setTimeout(function() {
+				$(chk).removeClass('collapsed');
+				$(chk).text(txt1);
+			}, 3000);
+		}
+	};
+	changeContext(chkContent, message, message2);
+	changeContext(chkName, author, author2);
+	setInterval(function() {
+		changeContext(chkContent, message, message2);
+	}, 6000);
+	setInterval(function() {
+		changeContext(chkName, author, author2);
+	}, 6000);
 });
