408d40db48774365d968f5ea5f30ae1ba081a7de
unknown <maryam_lfc@hotmail.com>
2021-05-24 19:11:50 +0300

    Red Square project

---
+++ 9.RedSquare/index.js
@@ -0,0 +1,29 @@
+let timer;
+
+const images = [
+	"url('./images/bg1.jpg')",
+	"url('./images/bg2.jpg')",
+	"url('./images/bg3.jpg')",
+	"url('./images/bg4.jpg')",
+	"url('./images/bg5.jpg')",
+	"url('./images/bg6.jpg')",
+	"url('./images/bg7.jpg')",
+	"url('./images/bg8.jpg')",
+	"url('./images/bg9.jpg')",
+	"url('./images/bg10.jpg')",
+	"url('./images/bg11.jpg')",
+	"url('./images/bg12.jpg')"
+];
+const slider = document.getElementById('#slider');
+const img = document.getElementsByClassName('change-images-img');
+let i = 0;
+
+const automaticSlideshow = () => {
+	img[0].style.backgroundImage = images[i];
+	i == 5 ? (i = 0) : i++;
+};
+
+let startTime = new Date().getTime();
+window.onload = function() {
+	setInterval(automaticSlideshow, 250);
+};
