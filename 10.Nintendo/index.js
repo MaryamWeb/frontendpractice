5b286dcad5018f57574db3b20e9ca4e092b9cfe5
unknown <maryam_lfc@hotmail.com>
2021-06-02 17:48:46 +0300

    Footer and slider

--- 10.Nintendo/index.js
+++ 10.Nintendo/index.js
@@ -18,3 +18,31 @@ $(document).ready(function() {
 		});
 	}
 });
+
+let slideIndex = 1;
+
+const changeSlideArrow = (x) => {
+	showSlides((slideIndex += x));
+};
+const changeSlideDot = (x) => {
+	showSlides((slideIndex = x));
+};
+
+let slides = document.getElementsByClassName('slide');
+let dots = document.getElementsByClassName('slide-dot');
+
+const setCSS = (i) => {
+	for (slide of slides) slide.style.display = 'none';
+	for (slide of slides) slide.className = slide.className.replace('activeslide', '');
+	slides[i - 1].style.display = 'grid';
+	slides[i - 1].className += ' activeslide';
+
+	for (dot of dots) dot.className = dot.className.replace('active', '');
+	dots[i - 1].className += ' active';
+};
+
+const showSlides = (x) => {
+	if (x > slides.length) slideIndex = 1; //reset Next to first
+	if (x < 1) slideIndex = slides.length; //reset Prev to last
+	setCSS(slideIndex);
+};
